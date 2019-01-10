278b14426486c0b247fb9891f287fbffd041816f
Derek Mathewson <mathewson17@live.ca>
2019-01-09 23:42:04 -0700

    auth routes

--- services/users/manage.py
+++ services/users/manage.py
@@ -49,17 +49,12 @@ def test():
 
 
 @cli.command()
-def recreate_db():
-    db.drop_all()
-    db.create_all()
-    db.session.commit()
-
-
-@cli.command()
 def seed_db():
     """Seeds the database."""
-    db.session.add(User(username='michael', email="hermanmu@gmail.com"))
-    db.session.add(User(username='michaelherman', email="michael@mherman.org"))
+    db.session.add(User(username='michael',
+                        email="hermanmu@gmail.com", password='greaterthaneight'))
+    db.session.add(User(username='michaelherman',
+                        email="michael@mherman.org", password='greaterthaneight'))
     db.session.commit()
 
 if __name__ == '__main__':
